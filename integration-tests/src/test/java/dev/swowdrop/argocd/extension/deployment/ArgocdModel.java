e5acddeccaaf88b3ea32c6ef97158b2fb7c9bb2b
cmoulliard <cmoulliard@redhat.com>
2025-02-17 14:56:11 +0100

    Add Argocd POJO class
    Signed-off-by: cmoulliard <cmoulliard@redhat.com>

---
+++ integration-tests/src/test/java/dev/swowdrop/argocd/extension/deployment/ArgocdModel.java
@@ -0,0 +1,11 @@
+package dev.swowdrop.argocd.extension.deployment;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class ArgocdModel {
+    public String username;
+    public String password;
+}
